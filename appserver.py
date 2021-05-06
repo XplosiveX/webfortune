05fa56147e6e26195bb1e192715b1f955ea1206b
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-05-05 20:42:56 -0500

    Update appserver.py
    rewrote the whole thing absolutely nothing worked right started reading output of commandline instead of text files <3

--- appserver.py
+++ appserver.py
@@ -1,69 +1,32 @@
-import flask
-from flask import render_template, request, session, redirect, url_for, jsonify, abort
+from flask import Flask, render_template, request, session, redirect, url_for, jsonify, abort
 import os
-import uuid
+import subprocess
+import uuid 
 
-app = flask(__name__)
-app.secret_key = uuid.uuid4().hex
-
-##tests to make sure files exist.
-try:
-    cow = open("cowsay.txt")
-    fortune = open("fortune.txt")
-    cow.close()
-    fortune.close()
-    # Do something with the file
-except:
-    try:
-        open("cowsay.txt", "x")
-    except:
-        pass
-    try:
-        open("fortune.txt", "x")
-    except:
-        pass
-returnvalue = ""
-#this is supposed to load an index.html and render
-##@app.route('/')
-##def hello():
-    ##return render_template('index.html', title='Welcome', members=users)
-
+app = Flask("web_fortune")
+app.secret_key = str(uuid.uuid4().hex)
 
 @app.route('/')
 def index():
     return redirect(url_for('fortune'))
 
-@app.route('/fortune/', methods=['GET'])
-def fortune():
-    os.system("fortune >> fortune.txt")
-    with open("fortune.txt", "r") as file:
-        returnvalue = file.read()
-        valueof = "<pre>" + returnvalue + "</pre>"
-        closer = open("fortune.txt","w")
-    closer.close()
-    returnvalue.close()
-    return valueof
+@app.route('/fortune/')
+def fortunenormal():
+    fortune = subprocess.check_output(["fortune"]).decode()
+    prereturn = "<pre>" + fortun + "</pre>"
+    return prereturn
 
-@app.route('/cowsay/<string:message>/', methods=['GET'])
-def cowsay(message):
-    os.system("cowsay " + message + ">> cowsay.txt")
-    with open("fortune.txt", "r") as file:
-        returnvalue = file.read()
-        valueof = "<pre>" + returnvalue + "</pre>"
-        closer = open("cowsay.txt","w")
-    closer.close()
-    returnvalue.close()
-    return valueof
+@app.route('/cowsay/<message>/')
+def cowsaynormal(message):
+    arg_list = ["cowsay"] + message.split()
+    cow= subprocess.check_output(arg_list).decode()
+    prereturn = "<pre>" + cow + "</pre>"
+    return prereturn
 
-
-@app.route('/cowfortune/', methods=['GET'])
-def cowfortune():
-    # cowsay stuff
-    os.system("cowsay `fortune` >> cowsay.txt")
-    with open("cowsay.txt", "r") as file:
-        returnvalue = file.read()
-        valueof = "<pre>" + returnvalue + "</pre>"
-        closer = open("cowsay.txt","w")
-    closer.close()
-    returnvalue.close()
-    return valueof
+@app.route('/cowfortune/')
+def cowsayfortune():
+    outcollect = subprocess.check_output(["fortune"]).decode()
+    argtodecode = ["cowsay"] + outcollect.split()
+    fortunecow = subprocess.check_output(argtodecode).decode()
+    prereturn = "<pre>" + fortunecow + "</pre>"
+    return prereturn

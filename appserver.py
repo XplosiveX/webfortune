12149cade2bce3ac7d1bd14d78f6bfff87a60698
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-05-05 21:03:40 -0500

    Update appserver.py

--- appserver.py
+++ appserver.py
@@ -6,6 +6,7 @@ import uuid
 app = Flask(__name__)
 app.secret_key = str(uuid.uuid4().hex)
 intropre = '<pre style="border:black solid 4px; border-radius: 12.5px; background:silver; opacity: 0.65; margin-left:auto; margin-right:auto;height:100%;height:65%;overflow:auto; text-align:center; font-size:16px;">'
+
 @app.route('/')
 def index():
     return redirect(url_for('fortune'))

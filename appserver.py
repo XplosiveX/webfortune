ebeee2b8c5dc34c3d550f0e7778316f598dafbf6
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-05-05 21:03:31 -0500

    Update appserver.py

--- appserver.py
+++ appserver.py
@@ -5,7 +5,7 @@ import uuid
 
 app = Flask(__name__)
 app.secret_key = str(uuid.uuid4().hex)
-
+intropre = '<pre style="border:black solid 4px; border-radius: 12.5px; background:silver; opacity: 0.65; margin-left:auto; margin-right:auto;height:100%;height:65%;overflow:auto; text-align:center; font-size:16px;">'
 @app.route('/')
 def index():
     return redirect(url_for('fortune'))
@@ -13,14 +13,14 @@ def index():
 @app.route('/fortune/')
 def fortunenormal():
     fortune = subprocess.check_output(["fortune"]).decode()
-    prereturn = "<pre>" + fortune + "</pre>"
+    prereturn = intropre + fortune + "</pre>"
     return prereturn
 
 @app.route('/cowsay/<message>/')
 def cowsaynormal(message):
     arg_list = ["cowsay"] + message.split()
     cow= subprocess.check_output(arg_list).decode()
-    prereturn = "<pre>" + cow + "</pre>"
+    prereturn = intropre + cow + "</pre>"
     return prereturn
 
 @app.route('/cowfortune/')
@@ -28,5 +28,5 @@ def cowsayfortune():
     outcollect = subprocess.check_output(["fortune"]).decode()
     argtodecode = ["cowsay"] + outcollect.split()
     fortunecow = subprocess.check_output(argtodecode).decode()
-    prereturn = "<pre>" + fortunecow + "</pre>"
+    prereturn = intropre + fortunecow + "</pre>"
     return prereturn

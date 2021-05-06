fb32f870238fe9b084822373245a002a0597a804
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-05-05 19:53:10 -0500

    Update appserver.py

--- appserver.py
+++ appserver.py
@@ -3,12 +3,17 @@ import os
 
 app = Flask(__name__)
 
-#this is supposed to load an index.html and render
-
+##tests to make sure files exist.
+try:
+    f = open("filename.txt")
+    # Do something with the file
+except:
+    print("File not accessible")
 returnvalue = ""
-@app.route('/')
-def hello():
-    return render_template('index.html', title='Welcome', members=users)
+#this is supposed to load an index.html and render
+##@app.route('/')
+##def hello():
+    ##return render_template('index.html', title='Welcome', members=users)
 
 
 @app.route('/')
@@ -18,30 +23,34 @@ def index():
 @app.route('/fortune/', methods=['GET'])
 def fortune():
     os.system("fortune >> fortune.txt")
-    with open("fortune.txt", "r") as f:
-        returnvalue = f.read()
+    with open("fortune.txt", "r") as file:
+        returnvalue = file.read()
         valueof = "<pre>" + returnvalue + "</pre>"
-    os.system("rm -rf fortune.txt")
+        closer = open("fortune.txt","w")
+    closer.close()
     returnvalue.close()
-    return content
+    return valueof
 
 @app.route('/cowsay/<string:message>/', methods=['GET'])
 def cowsay(message):
-    os.system("cowsay " + message + " >> cowsay.txt")
-    with open("cowsay.txt", "r") as f:
-        content = f.read()
-        content = "<pre>" + content + "</pre>"
-    os.system("rm -rf cowsay.txt")
-    return content
+    os.system("cowsay " + message + ">> cowsay.txt")
+    with open("fortune.txt", "r") as file:
+        returnvalue = file.read()
+        valueof = "<pre>" + returnvalue + "</pre>"
+        closer = open("cowsay.txt","w")
+    closer.close()
+    returnvalue.close()
+    return valueof
 
 
 @app.route('/cowfortune/', methods=['GET'])
 def cowfortune():
     # cowsay stuff
     os.system("cowsay `fortune` >> cowsay.txt")
-    with open("cowsay.txt", "r") as f:
-        outcontent = f.read()
-        outcontent = "<pre>" + outcontent + "</pre>"
-    os.system("rm -rf cowsay.txt")
-
-    return outcontent
+    with open("cowsay.txt", "r") as file:
+        returnvalue = file.read()
+        valueof = "<pre>" + returnvalue + "</pre>"
+        closer = open("cowsay.txt","w")
+    closer.close()
+    returnvalue.close()
+    return valueof

76ad0816946d9f761f95093c3e28d3e6b73cc02a
Dahlen Bryce <47333926+XplosiveX@users.noreply.github.com>
2021-05-05 20:10:43 -0500

    Update appserver.py

--- appserver.py
+++ appserver.py
@@ -1,4 +1,4 @@
-from flask import flask , render_template, request, session, redirect, url_for, jsonify, abort
+from flask import Flask, flask , render_template, request, session, redirect, url_for, jsonify, abort
 import os
 
 app = Flask(__name__)
